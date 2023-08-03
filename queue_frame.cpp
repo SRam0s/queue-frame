8eb3d278d23134be4b051550adc8d28d275f7c35
sram0s <sramos@163.com>
2023-08-03 22:09:00 +0800

    v1.1

--- queue_frame.cpp
+++ queue_frame.cpp
@@ -15,13 +15,9 @@ QueueFrame::~QueueFrame()
 
 int QueueFrame::start()
 {
-    // std::cout << "enter start" << std::endl;
-
     std::unique_lock<std::mutex> lock(this->start_mutex);
     this->condition_start.wait(lock,
         [this]{ return (this->enqueue_tasks_remaining == 0); });
-
-    // std::cout << "pass start" << std::endl;
 
     for(int i = 0; i < tasks_vec.size(); i++)
     {
@@ -33,16 +29,12 @@ int QueueFrame::start()
                 ret_vec.emplace_back(this->tasks_vec[i][j].get_future());
                 tasks.emplace_back(std::move(this->tasks_vec[i][j]));
 
-                //  std::cout << "prepare to notify_one" << std::endl;
-
-
                 condition.notify_one();
             }
         }
         for(int j = 0; j < tasks_vec[i].size(); j++)
         {
             ret_vec[j].wait();
-            // std::cout << "pass" << std::endl;
         }
 
         ret_vec.clear();
